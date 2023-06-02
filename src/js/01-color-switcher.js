0e808a21136d43563a7c5f878e0674c8d5f67cd4
vdbeast <122833284+vdbeast@users.noreply.github.com>
2023-06-02 15:01:24 +0300

    completed task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body')
+
+function changeBackgroundColor() {
+    const randomColor = getRandomHexColor();
+    bodyEl.style.backgroundColor = randomColor;
+}
+
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  timerId = setInterval(() => {
+  changeBackgroundColor()
+  }, 1000);
+})
+    
+
+stopBtn.addEventListener("click", () => {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(timerId);
+});
 
