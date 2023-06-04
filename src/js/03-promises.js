d82672a6960584a0b5560c213375488d4aea3fc1
vdbeast <122833284+vdbeast@users.noreply.github.com>
2023-06-04 22:10:20 +0300

    completed task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const delayInput = document.querySelector('input[name="delay"]');
+  const stepInput = document.querySelector('input[name="step"]');
+  const amountInput = document.querySelector('input[name="amount"]');
+
+  let currentDelay = Number(delayInput.value);
+  const step = Number(stepInput.value);
+  const amount = Number(amountInput.value);
+
+  for (let i = 0; i < amount; i++) {
+    createPromise(i + 1, currentDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    currentDelay += step;
+  }
+});
